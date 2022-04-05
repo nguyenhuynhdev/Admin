175f2ca4163361f9aef2014555386c9b3aef15c6
NguyenHuynh <nguyenhuynhdev@gmail.com>
2022-04-05 20:01:53 +0700

    Add project files.

---
+++ User Controls/Order.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Admin.User_Controls
+{
+
+    public partial class Order : UserControl
+    {
+        public Order()
+        {
+            InitializeComponent();
+        }
+
+        public String Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(Order));
+
+        public String Description
+        {
+            get { return (string)GetValue(DescriptionProperty); }
+            set { SetValue(DescriptionProperty, value); }
+        }
+
+        public static readonly DependencyProperty DescriptionProperty = DependencyProperty.Register("Description", typeof(string), typeof(Order));
+
+        public FontAwesome.Sharp.IconChar Icon
+        {
+            get { return (FontAwesome.Sharp.IconChar)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(FontAwesome.Sharp.IconChar), typeof(Order));
+    }
+}
